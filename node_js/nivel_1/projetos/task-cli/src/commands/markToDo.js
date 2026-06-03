22b69d76d32b520d95bbc2c6c09bb0b2e5044900
Ygor Santos <santosygor2007@gmail.com>
2026-06-03 14:10:54 -0300

    feat: add task-cli submodule

--- node_js/nivel_1/projetos/task-cli/src/commands/markToDo.js
+++ node_js/nivel_1/projetos/task-cli/src/commands/markToDo.js
@@ -1,15 +1,15 @@
-import {add, list} from "../storage.js";
-import { findIndex, verifyIndex } from "../helpers.js";
 import Error404 from "../error/Error404.js";
+import { findIndex, verifyIndex } from "../helpers.js";
+import { add, list } from "../storage.js";
 
-export default async function updateTask(id, description){
+export default async function markToDo(id){
   const date = new Date().toLocaleString();
   const tasks = await list();
   const index = findIndex(tasks, id);
   if(!verifyIndex(index)){
     throw new Error404();
   }
-  tasks[index].description = description;
+  tasks[index].status = "todo";
   tasks[index].updatedAt = date;
   await add(tasks, id);
 }
