5cf9d5fdadce2cc149d78587502976ad5c8c4082
Ygor Santos <santosygor2007@gmail.com>
2026-06-03 14:27:18 -0300

    fix: re-add task-cli as submodule

--- node_js/nivel_1/projetos/task-cli/src/commands/delete.js
+++
@@ -1,9 +0,0 @@
-import listTasks from "./list.js";
-import { add } from "../storage.js";
-
-
-export default async function deleteTask(id){
-  const tasks = await listTasks();
-  const updatedList = tasks.filter(task => task.id !== Number(id)); // remove a tarefa desejada do array 
-  await add(updatedList, id);
-}
--- node_js/nivel_1/projetos/task-cli/src/commands/list.js
+++
@@ -1,5 +0,0 @@
-import { list } from "../storage.js";
-
-export default async function listTasks(){
-  return await list();
-}
